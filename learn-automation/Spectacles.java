bcfc6874918bf348ff798a03d5e56ab99e102312
learnonlinejava <learnonline714@gmail.com>
2023-02-18 16:56:29 +0530

    first_commit

---
+++ learn-automation/Spectacles.java
@@ -0,0 +1,22 @@
+public class Spectacles {
+
+ static String BrandName = "Lenskart";
+
+ static double Range = 4.5;
+
+ static int Pair_Of_Glasses =  4;
+
+ static boolean Not_Readable = false;
+
+ public static void main(String[] args){
+
+  System.out.println("BrandName ="+BrandName);
+  System.out.println("Range ="+Range);
+  System.out.println("Pair_Of_Glasses ="+Pair_Of_Glasses);
+  System.out.println("Not_Readable ="+Not_Readable);
+
+ }
+
+}
+
+
---
+++ learn-automation/SpikeBuster.java
@@ -0,0 +1,22 @@
+public class SpikeBuster {
+
+ static String SpikeName = "Exide";
+
+ static double TotalLength = 2.5;
+
+ static int Number_of_ports =  4;
+
+ static boolean Switch_Option = true;
+
+ public static void main(String[] args){
+
+  System.out.println("SpikeName ="+SpikeName);
+  System.out.println("TotalLength ="+TotalLength);
+  System.out.println("Number_of_ports ="+Number_of_ports);
+  System.out.println("Switch_Option ="+Switch_Option);
+
+ }
+
+}
+
+
